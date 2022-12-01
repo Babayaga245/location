7fffb5a58218310331a298631308b8b86adae4eb
Babayaga245 <rayenzaalouni245@gmail.com>
2022-12-01 17:14:45 +0100

    Create location

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/location";
+        String username="root";
+        String password="";
+        try {
+            Class.forName ("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+        /*Statement statement=connection.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from client");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1)+" "+resultSet.getString(2)+" "+resultSet.getInt(6));
+            }*/
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
