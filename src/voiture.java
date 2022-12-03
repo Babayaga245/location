2875f03f2fe71d109e9e4b7c8c8f03eef47b2634
Babayaga245 <rayenzaalouni245@gmail.com>
2022-12-03 17:18:00 +0100

    modified

--- src/voiture.java
+++ src/voiture.java
@@ -1,3 +1,5 @@
+import javax.print.DocFlavor;
+
 public class voiture {
     private String matricule;
     private String modele;
@@ -11,5 +13,23 @@ public class voiture {
         this.matricule=matricule;
         this.modele=modele;
         this.prix=prix;
+    }
+    public void SetMat(String matricule){
+        this.matricule=matricule;
+    }
+    public String GetMat(){
+        return(this.matricule);
+    }
+    public void SetPrix(int prix){
+        this.prix=prix;
+    }
+    public float GetPrix(){
+        return(this.prix);
+    }
+    public void SetMod(String modele){
+        this.modele=modele;
+    }
+    public String GetMod(){
+        return(this.modele);
     }
 }
