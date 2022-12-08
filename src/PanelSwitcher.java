9d302287e2adbdf0b2d3f8fe3f6c4a288d20c040
BahaEddineFarhati <bahaeddine.farhati1@gmail.com>
2022-12-08 17:02:02 +0100

    UI (panels)

--- src/PanelSwitcher.java
+++ src/PanelSwitcher.java
@@ -1,44 +1,42 @@
 public class PanelSwitcher {
 
+    static Panel2 current_panel = Panel_main.Vehicule_panel;
+
     public static void Pan_switch(int a)
     {
         switch (a) {
 
             case 1 :
                 Panel_main.Vehicule_panel.main_panel.setVisible(true);
-                Panel_main.Client_panel.main_panel.setVisible((false));
-                Panel_main.loca_panel.main_panel.setVisible((false));
-                Panel_main.fac_panel.main_panel.setVisible((false));
-                Panel_main.info_panel.main_panel.setVisible((false));
+                if(current_panel != Panel_main.Vehicule_panel) {
+                    current_panel.main_panel.setVisible(false);
+                    current_panel = Panel_main.Vehicule_panel;
+                }
 
                 break;
             case 2 :
                 Panel_main.Client_panel.main_panel.setVisible((true));
-                Panel_main.Vehicule_panel.main_panel.setVisible(false);
-                Panel_main.loca_panel.main_panel.setVisible((false));
-                Panel_main.fac_panel.main_panel.setVisible((false));
-                Panel_main.info_panel.main_panel.setVisible((false));
+                if(current_panel != Panel_main.Client_panel){
+                current_panel.main_panel.setVisible(false);
+                current_panel = Panel_main.Client_panel;}
                 break;
             case 3 :
-                Panel_main.Vehicule_panel.main_panel.setVisible(false);
-                Panel_main.Client_panel.main_panel.setVisible((false));
                 Panel_main.loca_panel.main_panel.setVisible((true));
-                Panel_main.fac_panel.main_panel.setVisible((false));
-                Panel_main.info_panel.main_panel.setVisible((false));
+                if(current_panel != Panel_main.loca_panel){
+                current_panel.main_panel.setVisible(false);
+                current_panel = Panel_main.loca_panel;}
                 break;
             case 4 :
-                Panel_main.Vehicule_panel.main_panel.setVisible(false);
-                Panel_main.Client_panel.main_panel.setVisible((false));
-                Panel_main.loca_panel.main_panel.setVisible((false));
                 Panel_main.fac_panel.main_panel.setVisible((true));
-                Panel_main.info_panel.main_panel.setVisible((false));
+                if(current_panel != Panel_main.Vehicule_panel){
+                current_panel.main_panel.setVisible(false);
+                current_panel = Panel_main.fac_panel;}
                 break;
             case 5 :
-                Panel_main.Vehicule_panel.main_panel.setVisible(false);
-                Panel_main.Client_panel.main_panel.setVisible((false));
-                Panel_main.loca_panel.main_panel.setVisible((false));
-                Panel_main.fac_panel.main_panel.setVisible((false));
                 Panel_main.info_panel.main_panel.setVisible((true));
+                if(current_panel != Panel_main.Vehicule_panel){
+                current_panel.main_panel.setVisible(false);
+                current_panel = Panel_main.info_panel;}
                 break;
 
 
--- src/Panel_main.java
+++ src/Panel_main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 
 public class Panel_main {
@@ -7,9 +8,221 @@ public class Panel_main {
     static Panel2 loca_panel;
     static Panel2 fac_panel;
     static Panel2 info_panel;
+
+    private static void Vehicule_panel_init()
+    {
+
+
+
+
+
+        JTable jTable1 = new javax.swing.JTable();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+                new Object [][] {
+
+                },
+                new String [] {
+                        "Modele de voiture", "Matricule", "prix", "Location"
+                }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                    false, false, true, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+
+
+
+         JScrollPane sp = new JScrollPane(jTable1);
+
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setResizable(false);
+            jTable1.getColumnModel().getColumn(1).setResizable(false);
+            jTable1.getColumnModel().getColumn(2).setResizable(false);
+            jTable1.getColumnModel().getColumn(3).setResizable(false);
+        }
+        Vehicule_panel.main_panel.add(sp);
+
+
+
+
+
+
+
+        JButton jButton6 = new JButton();
+
+        JButton jButton7 = new JButton();
+        JButton jButton8 = new JButton();
+        JButton jButton9 = new JButton();
+        JButton jButton10 = new JButton();
+
+        jButton8.setText("jButton8");
+
+        jButton9.setText("jButton9");
+
+        jButton10.setText("jButton10");
+        JTextField jTextField1 = new JTextField();
+        jTextField1.setText("jTextField1");
+       /* jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });*/
+        JLabel jLabel2 = new JLabel();
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel2.setText("Modele");
+        JTextField jTextField2 = new JTextField();
+        jTextField2.setText("jTextField1");
+       /* jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });*/
+        JLabel jLabel3 = new JLabel();
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel3.setText("Matricule");
+
+        JTextField jTextField3 = new JTextField();
+        jTextField3.setText("jTextField1");
+        /*jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });*/
+        JLabel jLabel4 = new JLabel();
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel4.setText("Prix");
+        JButton jButton11 = new JButton();
+        jButton11.setText("ADD");
+        /*jButton11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton11ActionPerformed(evt);
+            }
+        });*/
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(Vehicule_panel.main_panel);
+        Vehicule_panel.main_panel.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+                jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel4Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(jPanel4Layout.createSequentialGroup()
+                                                .addGap(81, 81, 81)
+                                                .addComponent(jButton11)))
+                                .addGap(37, 37, 37)
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(jPanel4Layout.createSequentialGroup()
+                                                .addGap(4, 4, 4)
+                                                .addComponent(jButton6)
+                                                .addGap(12, 12, 12)
+                                                .addComponent(jButton7)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(jButton8)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(jButton9)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(jButton10))
+                                        .addComponent(sp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(308, Short.MAX_VALUE))
+        );
+        jPanel4Layout.setVerticalGroup(
+                jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel4Layout.createSequentialGroup()
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(jPanel4Layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(sp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(jButton10, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addGroup(jPanel4Layout.createSequentialGroup()
+                                                .addGap(50, 50, 50)
+                                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(50, 50, 50)
+                                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(50, 50, 50)
+                                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(33, 33, 33)
+                                                .addComponent(jButton11, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addContainerGap(24, Short.MAX_VALUE))
+        );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
     public static void Panel_Main_Start(Color p1_Col,Color p2_Col)
     {
+
         Vehicule_panel = new Panel2(p1_Col,"Vehicule");
+        Vehicule_panel_init();
+        Vehicule_panel.main_panel.setVisible(true);
         Client_panel = new Panel2(p2_Col,"Client");
         loca_panel = new Panel2(p2_Col,"Location");
         fac_panel = new Panel2(p2_Col,"facture");
@@ -17,7 +230,11 @@ public class Panel_main {
 
 
 
+
+
+
     }
+
 
 
 }
