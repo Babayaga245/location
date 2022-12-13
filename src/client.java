a37cc69f6da84b35bf8ab7192365c14439d20854
Babayaga245 <rayenzaalouni245@gmail.com>
2022-12-13 16:45:21 +0100

    added afficher(cars+clients)

--- src/client.java
+++ src/client.java
@@ -1,21 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class client {
     private int numpermis;
-    private String nom;
-    private String prenom;
-    private String adresse;
+    private String nomprenom;
     private int tel;
     public client(){
         numpermis= Integer.parseInt(null);
-        nom=null;
-        prenom=null;
-        adresse=null;
+        nomprenom=null;
         tel= Integer.parseInt(null);
     }
-    public client(int numpermis,String nom,String prenom, String adresse, int tel){
+    public client(int numpermis,String nomprenom, int tel){
         this.numpermis=numpermis;
-        this.nom=nom;
-        this.prenom=prenom;
-        this.adresse=adresse;
+        this.nomprenom=nomprenom;
         this.tel=tel;
     }
     public void SetNumpermis(int numpermis){
@@ -24,23 +23,11 @@ public class client {
     public int GetNumpermis(){
         return(this.numpermis);
     }
-    public void SetNom(String nom){
-        this.nom=nom;
+    public void SetNomprenom(String nom){
+        this.nomprenom=nom;
     }
-    public String GetNom(){
-        return(this.nom);
-    }
-    public void SetPrenom(String prenom){
-        this.prenom=prenom;
-    }
-    public String GetPrenom(){
-        return(this.prenom);
-    }
-    public void SetAdresse(String adresse){
-        this.adresse=adresse;
-    }
-    public String GetAdresse(){
-        return(this.adresse);
+    public String GetNomprenom(){
+        return(this.nomprenom);
     }
     public void SetTel(int tel){
         this.tel=tel;
@@ -48,5 +35,57 @@ public class client {
     public int GetTel(){
         return(this.tel);
     }
+    public static client[] Getclientrray(int n){
+        client arr[]=new client[200];
+        String url="jdbc:mysql://localhost:3306/location";
+        String username="root";
+        String password="";
+        try {
+            Class.forName ("com.mysql.cj.jdbc.Driver");
 
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement=connection.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from client");
+            while (resultSet.next()){
+                client v = new client(resultSet.getInt(1),resultSet.getString(2),resultSet.getInt(3));
+                arr[n]=v;
+                n++;
+            }
+
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            //test
+        }
+
+        return(arr);
+
+    }
+    public static void addClient(client c){
+        String url="jdbc:mysql://localhost:3306/location";
+        String username="root";
+        String password="";
+        try {
+            Class.forName ("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement=connection.createStatement();
+            String sql = "insert into client (numpermis,nomprenom,tel) values('" + c.GetNumpermis() + "','"
+                    + c.GetNomprenom() + "','" + c.GetTel()  + "');";
+            System.out.println(sql);
+            statement = connection.createStatement();
+            int i = statement.executeUpdate(sql);
+            if (i > 0) {
+                System.out.println("ROW INSERTED");
+            } else {
+                System.out.println("ROW NOT INSERTED");
+            }
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            //test
+        }
+    }
 }
--- src/voiture.java
+++ src/voiture.java
@@ -1,4 +1,8 @@
 import javax.print.DocFlavor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class voiture {
     private String matricule;
@@ -31,5 +35,57 @@ public class voiture {
     }
     public String GetMod(){
         return(this.modele);
+    }
+    public static voiture[] GetCarArray(int n){
+        voiture cars[]=new voiture[200];
+        String url="jdbc:mysql://localhost:3306/location";
+        String username="root";
+        String password="";
+        try {
+            Class.forName ("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement=connection.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from voiture");
+            while (resultSet.next()){
+                voiture v = new voiture(resultSet.getString(1),resultSet.getString(2),resultSet.getFloat(3));
+                cars[n]=v;
+                n++;
+            }
+
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            //test
+        }
+
+        return(cars);
+
+    }
+    public static void addCar(voiture c){
+        String url="jdbc:mysql://localhost:3306/location";
+        String username="root";
+        String password="";
+        try {
+            Class.forName ("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement=connection.createStatement();
+            String sql = "insert into voiture (matricule,modele,prix) values('" + c.GetMat() + "','"
+                    + c.GetMod() + "','" + c.GetPrix()  + "');";
+            statement = connection.createStatement();
+            int i = statement.executeUpdate(sql);
+            if (i > 0) {
+                System.out.println("ROW INSERTED");
+            } else {
+                System.out.println("ROW NOT INSERTED");
+            }
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            //test
+        }
     }
 }
